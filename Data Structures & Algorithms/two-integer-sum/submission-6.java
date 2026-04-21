8e1ecfc45187446fd4a5985f747c3ec44c393fc6
Andrii Synytsia <asynytsia.work@gmail.com>
2026-04-20 19:56:36 -0600

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {        
+        for(int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                int res = nums[i] + nums[j];
+                if(res == target && j != i) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[2];
+    }
+}
